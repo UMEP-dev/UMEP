ec5f01a04eede18fb6942ebb10d34c697780e453
Ting Sun <sunting.05@gmail.com>
2020-01-31 17:00:42 +0000

    implement auto installation of supy

--- suewsmodel/__init__.py
+++ suewsmodel/__init__.py
@@ -1,2 +1,7 @@
 # -*- coding: utf-8 -*-
 # __author__ = 'xlinfr'
+
+from .supy_installer import setup_supy
+# we can specify a version if needed
+setup_supy(ver=None)
+# setup_supy(ver='2020.1.23')
---
+++ suewsmodel/supy_installer.py
@@ -0,0 +1,93 @@
+import sys, subprocess, os
+from pathlib import Path
+import platform
+
+# locate QGIS-python interpreter
+def locate_py():
+    try:
+        # non-Linux
+        path_py = os.environ["PYTHONHOME"]
+    except Exception:
+        # Linux
+        path_py = sys.executable
+
+    # convert to Path for eaiser processing
+    path_py = Path(path_py)
+
+    # pre-defined paths for python executable
+    dict_pybin = {
+        "Darwin": path_py / "bin" / "python3",
+        "Windows": path_py / "python3.exe",
+        "Linux": path_py,
+    }
+
+    # python executable
+    path_pybin = dict_pybin[platform.system()]
+
+    if path_pybin.exists():
+        return path_pybin
+    else:
+        raise RuntimeError("UMEP cannot locate the Python interpreter used by QGIS!")
+
+
+# check if supy is installed
+def check_supy_version():
+    try:
+        path_pybin = locate_py()
+        list_cmd = f"{str(path_pybin)} -m pip show supy".split()
+        list_info = subprocess.check_output(list_cmd, encoding="UTF8").split("\n")
+        str_ver = list_info[1].split(":")[1].strip()
+        return str_ver
+    except Exception:
+        raise RuntimeError("UMEP cannot identify a supy installation!") from Exception
+
+
+# install supy
+def install_supy(ver=None):
+
+    str_ver = f"=={ver}" if ver else ""
+    try:
+        path_pybin = locate_py()
+        list_cmd = f"{str(path_pybin)} -m pip install supy{str_ver} -U --user".split()
+        list_info = subprocess.check_output(list_cmd, encoding="UTF8").split("\n")
+
+        str_info = list_info[-2].strip()
+        str_info = (
+            str_info
+            if len(str_info) > 0
+            else f"supy{str_ver} has already been installed!"
+        )
+        return str_info
+    except Exception:
+        raise RuntimeError(f"UMEP couldn't install supy{str_ver}!") from Exception
+
+
+# uninstall supy
+def uninstall_supy():
+
+    try:
+        path_pybin = locate_py()
+        list_cmd = f"{str(path_pybin)} -m pip uninstall supy -y".split()
+        list_info = subprocess.check_output(list_cmd, encoding="UTF8").split("\n")
+
+        str_info = list_info[-2].strip()
+        return str_info
+    except Exception:
+        raise RuntimeError(f"UMEP couldn't uninstall supy!") from Exception
+
+
+# set up supy
+def setup_supy(ver=None, debug=False):
+    if debug:
+        uninstall_supy()
+        install_supy(ver)
+
+    try:
+        # check if supy has been installed
+        import supy as sp
+
+        sp.show_version()
+
+    except Exception:
+        # install supy
+        install_supy(ver)
