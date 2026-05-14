9d7a683b48fb5c10f172e8f96048c804b94e61f2
biglimp <biglimp@users.noreply.github.com>
2026-05-14 11:16:27 +0200

    Update suews_simple.py
    Minor change to avoid error in QGIS 3.X

--- SuewsSimple/suews_simple.py
+++ SuewsSimple/suews_simple.py
@@ -22,6 +22,7 @@
 """
 
 from __future__ import print_function
+from __future__ import absolute_import
 from pathlib import Path
 import yaml
 import sys
@@ -41,7 +42,6 @@ from qgis.PyQt.QtCore import QSettings, QTranslator, qVersion, QCoreApplication
 from builtins import object
 from builtins import range
 from builtins import str
-from __future__ import absolute_import
 from future import standard_library
 
 standard_library.install_aliases()
